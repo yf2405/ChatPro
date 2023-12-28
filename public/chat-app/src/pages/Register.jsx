b0dff5292792ec568e937ae4cef0e04d736299fe
Yerson Felipe Correa <yersonstuardlittle062@gmail.com>
2023-12-27 22:00:00 -0500

    create design and  styles of register.

--- public/chat-app/src/pages/Register.jsx
+++ public/chat-app/src/pages/Register.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import Logo from "../assets/react.svg"
+
 export default function Register() {
     const handleSubmit = (event) => {
         event.preventDefault();
         alert("form");
     };
+    const handleChange = (event) => {
+
+    };
   return (
     <FormContainer>
         <form onSubmit={(event) => handleSubmit(event)}>
             <div className="brand">
-            <img src="" alt="" />
+            <img src={Logo} alt="logo" />
             <h1>chat</h1>
             </div>
             <input 
@@ -17,10 +23,99 @@ export default function Register() {
             placeholder="Username" 
             name="Username"
             onChange={(e) => handleChange(e)}
-            className=""/>
+           />
+            <input 
+            type="email" 
+            placeholder="Email" 
+            name="Email"
+            onChange={(e) => handleChange(e)}
+           />
+            <input 
+            type="password" 
+            placeholder="Password" 
+            name="Password"
+            onChange={(e) => handleChange(e)}
+           />
+            <input 
+            type="password"  
+            placeholder="Confirm Password"  
+            name="ConfirmPassword"
+            onChange={(e) => handleChange(e)}
+           />
+           <button type="submit">Create User</button>
+           <span>
+             Already have an account ? <Link to="/login">Login</Link>
+             </span>
         </form>
     </FormContainer>
-  )
+  ) 
 }
 
-const FormContainer = styled.div``;
+const FormContainer = styled.div`
+height:100vh;
+width:100vw;
+display:flex;
+flex-direction: column;
+justify-content: center;
+gap:1rem;
+align-items: center;
+background-color:#131324;
+.brand{
+  display:flex;
+  align-items: center;
+  gap:1rem;
+  justify-content:center;
+  img{
+    height:5rem;
+  }
+  h1{
+    color:white;
+    text-transform:uppercase;
+  }
+}
+form{
+  display:flex;
+  flex-direction: column;
+  gap:2rem;
+  background-color:#00000076;
+  border-radius:2rem;
+  padding:3rem 5rem;
+  input{
+    background-color:transparent;
+    padding:1rem;
+    border:0.1rem solid #4e0eff;
+    border-radius:0.4rem;
+    color:white;
+    width:100%;
+    font-size:1rem;
+    &:focus{
+      border:0.1rem solid #997af0;
+      outline:none;
+    }
+  }
+  button{
+    background-color:#997af0;
+    color:white;
+    padding:1rem 2rem;
+    border:none;
+    font-wight:bold;
+    cursor:pointer;
+    border-radius:0.4rem;
+    font-size:1rem;
+    text-transform:uppercase;
+    transition:0.5s ease-in-out;
+    &:hover{
+      background-color:#4e0eff;
+    }
+  }
+  span{
+    color:white;
+    text-transform:uppercase;
+    a{
+      color:#4e0eff;
+      text-decoration:none;
+      font-weight:bold;
+    }
+  }
+}
+`
