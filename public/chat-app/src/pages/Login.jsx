1e4008713a3114e802bdca7e8274349a96a3a064
Yerson Felipe Correa <yersonstuardlittle062@gmail.com>
2024-01-04 22:10:57 -0500

    create register functionality

--- public/chat-app/src/pages/Login.jsx
+++ public/chat-app/src/pages/Login.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
-
+import React from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 export default function Login() {
+  const showToastMessage = () => {
+    toast.success("Success Notification !", {
+      position: toast.POSITION.TOP_RIGHT,
+    });
+  };
+
   return (
-    <div>Login</div>
-  )
+    <div>
+      <button onClick={showToastMessage}>Notify</button>
+      <ToastContainer />
+    </div>
+  );
 }
+
+
--- public/chat-app/src/pages/Register.jsx
+++ public/chat-app/src/pages/Register.jsx
@@ -1,53 +1,114 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
-import Logo from "../assets/react.svg"
-
+import { Link, useNavigate } from 'react-router-dom';
+import Logo from "../assets/react.svg";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import { registerRoute } from '../../utils/APIRoutes';
 export default function Register() {
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        alert("form");
-    };
+  const navigate = useNavigate()
+  const [values, setValues] = useState({
+    username:"",
+    email:"",
+    password:"",
+    confirmPassword:"",
+  });
+  // configuration for  the alert app with toast of react-toastify
+  const toastOptions = {
+    position: "bottom-left",
+    autoClose: 8000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
+  }
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    
+    if (handleValidation()) {
+      
+      const { password, username, email } = values;
+  
+        const {data} = await axios.post(registerRoute, {
+          username,
+          email,
+          password,
+        });
+        if(data.status === false) {
+          toast.error(data.msg,toastOptions);
+        }
+        if(data.status === true) {
+          localStorage.setItem('chat-app-user',JSON.stringify(data.user));
+          navigate("/");
+        }
+        
+    }
+  };
     const handleChange = (event) => {
-
+     setValues({...values, [event.target.name]: event.target.value})
     };
+    const handleValidation = () => {
+      const { password, confirmPassword, username, email } = values;
+      
+      if (password !== confirmPassword) {
+        toast.error("Password and confirm password should be same !", toastOptions);
+        return false;
+      } else if (username.length < 3) {
+        toast.error("Username must be at least 3 characters", toastOptions);
+        return false;
+      } else if (password.length < 8) {
+        toast.error("Password must be at least 8 characters", toastOptions);
+        return false;
+      } else if (!email) {
+        toast.error("Email is required", toastOptions);
+        return false;
+      }
+    
+      return true;
+    };
+    
+
   return (
+    <>
     <FormContainer>
         <form onSubmit={(event) => handleSubmit(event)}>
             <div className="brand">
             <img src={Logo} alt="logo" />
             <h1>chat</h1>
             </div>
-            <input 
-            type="text" 
-            placeholder="Username" 
-            name="Username"
-            onChange={(e) => handleChange(e)}
-           />
-            <input 
-            type="email" 
-            placeholder="Email" 
-            name="Email"
-            onChange={(e) => handleChange(e)}
-           />
-            <input 
-            type="password" 
-            placeholder="Password" 
-            name="Password"
-            onChange={(e) => handleChange(e)}
-           />
-            <input 
-            type="password"  
-            placeholder="Confirm Password"  
-            name="ConfirmPassword"
-            onChange={(e) => handleChange(e)}
-           />
+            <input
+  type="text"
+  placeholder="Username"
+  name="username"
+  onChange={(e) => handleChange(e)}
+/>
+<input
+  type="email"
+  placeholder="Email"
+  name="email"
+  onChange={(e) => handleChange(e)}
+/>
+<input
+  type="password"
+  placeholder="Password"
+  name="password"
+  onChange={(e) => handleChange(e)}
+/>
+<input
+  type="password"
+  placeholder="Confirm Password"
+  name="confirmPassword"
+  onChange={(e) => handleChange(e)}
+/>
+
            <button type="submit">Create User</button>
            <span>
              Already have an account ? <Link to="/login">Login</Link>
              </span>
         </form>
     </FormContainer>
+    <ToastContainer/>
+    </>
   ) 
 }
 
@@ -98,7 +159,7 @@ form{
     color:white;
     padding:1rem 2rem;
     border:none;
-    font-wight:bold;
+    font-weight:bold;
     cursor:pointer;
     border-radius:0.4rem;
     font-size:1rem;
