01d0058e752f2548b71d617ac40d2ab087715199
Yerson Felipe Correa <yersonstuardlittle062@gmail.com>
2024-01-07 21:30:31 -0500

    set avatar / profile picture/ picking avatar

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -40,3 +40,19 @@ module.exports.login = async (req,res,next) => {
     }
     };
     
+module.exports.setAvatar = async (req, res, next) => {
+    try{
+      const userId = req.params.id;
+      const avatarImage = req.body.image;
+      const userData = await User.findByIdAndUpdate(userId, {
+      isAvatarImageSet:true,
+      avatarImage,
+      });
+      return res.json({
+        isSet: userData.isAvatarImageSet, 
+        image: userData.avatarImage,
+    });
+    }catch(error) {
+    next(error);
+}
+}
