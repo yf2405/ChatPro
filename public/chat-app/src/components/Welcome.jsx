6d30796a1f456d84127c8a48345c4ffb2e53d9c9
Yerson Felipe Correa <yersonstuardlittle062@gmail.com>
2024-01-17 22:22:57 -0500

    all the init of chat

---
+++ public/chat-app/src/components/Welcome.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import styled from "styled-components";
+import Robot from "../assets/robot.gif";
+export default function Welcome({ currentUser }) {
+    console.log(currentUser)
+  return (
+    <Container>
+        <img src={Robot} alt="Robot" />
+        <h1>
+            welcome, <span>{currentUser?.username}!</span>
+        </h1>
+       <h3>por favor comienza a mensagiar.</h3>
+
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  flex-direction: column;
+  img {
+    height: 20rem;
+  }
+  span {
+    color: #4e0eff;
+  }
+`;
--- public/chat-app/src/pages/Chat.jsx
+++ public/chat-app/src/pages/Chat.jsx
@@ -1,7 +1,88 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
+import styled from 'styled-components';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import Contacts from '../components/Contacts';
+import { allUsersRoute } from '../../utils/APIRoutes';
+import Welcome from '../components/Welcome';
 
 export default function Chat() {
+  const navigate = useNavigate();
+  const [contacts, setContacts] = useState([]);
+  const [currentUser, setCurrentUser] = useState(undefined);
+  const [currentChat, setCurrentChat] = useState(undefined);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (!localStorage.getItem("chat-app-user")) {
+          navigate("/login");
+        } else {
+          setCurrentUser(await JSON.parse(localStorage.getItem("chat-app-user")));
+        }
+      } catch (error) {
+       
+      }
+    };
+  
+    fetchData();
+  }, []);
+  
+  useEffect(() => {
+    const fetchContacts = async () => {
+      try {
+        if (currentUser) {
+          if (currentUser.isAvatarImageSet) {
+            const data = await axios.get(`${allUsersRoute}/${currentUser._id}`);
+            setContacts(data.data);
+          } else {
+            navigate("/setAvatar");
+          }
+        }
+      } catch (error) {
+      
+      }
+    };
+  
+    fetchContacts();
+  }, [currentUser]);
+
+  const handleChatChange = (chat) => {
+    setCurrentChat(chat);
+  };
   return (
-    <div>Chat</div>
-  )
+    <Container>
+      <div className="container">
+        <Contacts 
+        contacts={contacts} 
+        currentUser={currentUser} 
+        changeChat={handleChatChange}
+        />
+        <Welcome
+        currentUser={currentUser} 
+        />
+      </div>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+  .container {
+    height: 85vh;
+    width: 85vw;
+    background-color: #00000076;
+    display: grid;
+    grid-template-columns: 25% 75%;
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      grid-template-columns: 35% 65%;
+    }
+  }
+`;
